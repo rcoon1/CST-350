09ee61ec989976fc3940a02fd4f27309fb298e04
rcoon1 <78291341+rcoon1@users.noreply.github.com>
2023-06-24 12:17:14 -0500

    add activity 8

---
+++ Topic 8/LINQ-Demo/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQ_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var scores = new[] { 50, 66, 90, 81, 77, 45, 0, 100, 99, 72, 87, 85, 81, 80, 77, 74, 95, 97 };
+
+            foreach (var individualScore in scores)
+            {
+                Console.WriteLine("One of the scores was {0}", individualScore);
+            }
+            Console.ReadLine();
+
+            var theBestStudents =
+            from individualScore in scores
+            where individualScore > 90
+            select individualScore;
+
+            foreach (var individualScore in theBestStudents)
+            {
+                Console.WriteLine("One of the BEST scores was {0}", individualScore);
+            }
+            Console.ReadLine();
+
+            var sortedScores =
+            from individualScores in scores
+            orderby individualScores
+            select individualScores;
+
+            foreach (var individualScores in sortedScores)
+            {
+                Console.WriteLine("One of the scores was {0}", individualScores);
+            }
+            Console.ReadLine();
+
+            var bStudents =
+            from individualScores in scores
+            orderby individualScores ascending
+            where individualScores > 80 && individualScores < 90
+            select individualScores;
+            Console.WriteLine("#### Challenge #####");
+            foreach (var individualScores in bStudents)
+            {
+                Console.WriteLine("One of the B scores was {0}", individualScores);
+            }
+            Console.ReadLine();
+        }
+        
+    }
+}
---
+++ Topic 8/LINQActors/Actor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LINQActors
+{
+    internal class Actor
+    {
+        public string Name { get; set; }
+        public decimal TotalGross { get; set; }
+        public int MovieCount { get; set; }
+        public decimal AvgPerMovie { get; set; }
+        public string TopMovie { get; set; }
+        public decimal TopMovieGross { get; set; }
+
+
+
+        public override string ToString()
+        {
+            return "Name: " + Name +
+                (TotalGross != 0 ? " TotalGross: " + TotalGross : null) +
+                (MovieCount != 0 ? " MovieCount: " + MovieCount : null) +
+                (AvgPerMovie != 0 ? " AvgPerMovie: " + AvgPerMovie : null) +
+                (TopMovie != null ? " TopMovie: " + TopMovie : null) +
+                (TopMovieGross != 0 ? " TopMovieGross: " + TopMovieGross : null) +
+                "\n";
+        }
+    }
+}
